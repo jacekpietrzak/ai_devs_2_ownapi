43b7d572e58fc2b1c257e214f212314aae19ddd6
Jacek Pietrzak <jacekpietrzak@me.com>
2024-06-05 15:57:46 +0200

    chore: Update welcome message endpoint in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,7 @@ const router = Router();
 const answerRoute = require("./answerRoute");
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    message: "Welcome to the API",
-  });
+  res.send("Hello from the API");
 });
 
 router.use("/answer", answerRoute);
